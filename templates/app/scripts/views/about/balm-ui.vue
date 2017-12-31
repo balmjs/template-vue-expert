10d626fe307460073b801f4947ad8c93bd7a09e5
Elf-mousE <ifmouz@gmail.com>
2017-12-31 17:07:10 +0800

    update

--- templates/app/scripts/views/about/balm-ui.vue
+++ templates/app/scripts/views/about/balm-ui.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page--about-ui">
-    <h2><a href="//balmjs.com/ui-vue/">BalmUI</a></h2>
+    <h2><a href="//material.balmjs.com/">BalmUI</a></h2>
     <p>Material Components + Vue</p>
   </div>
 </template>
