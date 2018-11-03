af68c5f3eeddb9369970a43be168ccf5b4237d71
Elf-mousE <ifmouz@gmail.com>
2018-11-03 10:34:36 +0800

    update

--- templates/app/scripts/store/models/user.js
+++ templates/app/scripts/store/models/user.js
@@ -8,8 +8,8 @@ export default {
   },
   methods: {
     async getMenu() {
-      let resp = await axios.get('/data/menu.json');
-      let { code, data, message } = resp.data;
+      let response = await axios.get('/data/menu.json');
+      let { code, data, message } = response;
 
       if (code === 200) {
         this.menu = data;
