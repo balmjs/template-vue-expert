a80b9a1dcd16dd57e516cd4922295b07217962fc
Elf-mousE <ifmouz@gmail.com>
2020-10-22 14:29:02 +0800

    update config

--- templates/balm.config.js
+++ templates/balm.config.js
@@ -1,10 +1,13 @@
 const config = require('./config/balmrc');
 const publish = require('./config/balm.publish');
 
-const api = mix => {
+const api = (mix) => {
   if (mix.env.isProd) {
     // For test data
     mix.copy('./app/data/*', './dist/api');
+  } else {
+    // For BalmUI iconfonts
+    mix.copy('node_modules/balm-ui/fonts/*', 'app/fonts');
   }
 
   publish(mix);
